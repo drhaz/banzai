2d2a126495af1dd4e4ec67127414d8edb521e619
Daniel Harbeck <drharbeck@gmail.com>
2017-06-26 17:07:33 -0700

    First photzp fit.
    Warning: This is a very ugly prototype implementaion that need desperate integration into banzai scheme

--- banzai/photcalibration.py
+++ banzai/photcalibration.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+
+import matplotlib
+
 from banzai.stages import Stage
 from banzai import logs
+from banzai import images
 from banzai.utils import image_utils
 import os, subprocess, shlex
 from astropy.io import fits
@@ -67,7 +71,7 @@ class PS1Catalog:
         return table
 
     def panstarrs_query(self, ra_deg, dec_deg, rad_deg, mindet=5,
-                    maxsources=5,
+                    maxsources=50000,
                     server=('http://gsss.stsci.edu/webservices/vo/CatalogSearch.aspx')):
         """
         Query Pan-STARRS DR1 @ MAST
@@ -80,22 +84,94 @@ class PS1Catalog:
 
         copied from https://michaelmommert.wordpress.com/2017/02/13/accessing-the-gaia-and-pan-starrs-catalogs-using-python/
         """
-        r = requests.get(server,
-        params= {'CAT':"PS1V3OBJECTS", 'RA': ra_deg, 'DEC': dec_deg,
-             'SR': rad_deg, 'MAXOBJ': maxsources,
-             'FORMAT': 'CSV',
-             'ndetections': ('>%d' % mindet)})
-
+        # r = requests.get(server,
+        # params= {'CAT':"PS1V3OBJECTS", 'RA': ra_deg, 'DEC': dec_deg,
+        #      'SR': rad_deg, 'MAXOBJ': maxsources,
+        #      'FORMAT': 'CSV',
+        #      'ndetections': ('>%d' % mindet)})
+        #
+        # input = StringIO.StringIO(r.text)
         # write query data into local file
+        #outf = open('panstarrs.dat', 'w')
+        #outf.write(r.text)
+        #outf.close()
 
-        print (r.text + "\n")
-        table = np.genfromtxt (StringIO.StringIO(r.text),  names=True, skip_header=1, delimiter=',', )
+        input =  'panstarrs.dat'
+        table = np.genfromtxt (input,  names=True, skip_header=1, delimiter=',', )
 
-        print (table['iMeanPSFMag'][0])
+
         self.PS1toSDSS(table)
-        print (table['iMeanPSFMag'][0])
+
         return table
 
 # Example query
+
+from astropy import units as u
+
+def loadFitsCatalog (image):
+
+    testimage = fits.open ('../testing/coj1m011-fl12-20170613-0073-e11.fits.fz')
+
+    ra  = testimage['SCI'].header['CRVAL1']
+    dec = testimage['SCI'].header['CRVAL2']
+    exptime = testimage['SCI'].header['EXPTIME']
+    phot = testimage['CAT'].data
+    image_wcs = WCS(testimage['SCI'].header)
+    ras, decs = image_wcs.all_pix2world(phot['x'], phot['y'], 1)
+
+
+    return ra,dec,phot, ras, decs, exptime
+
+
+
+ra,dec,phot,ras,decs, exptime = loadFitsCatalog   (None)
 ps1 = PS1Catalog ()
-table = ps1.panstarrs_query(12.345, 67.89, 0.1)
+reftable = ps1.panstarrs_query(ra,dec,0.3)
+
+
+
+cphot = SkyCoord(ra=ras*u.degree, dec=decs*u.degree)
+cref  = SkyCoord(ra=reftable['raMean']*u.degree, dec=reftable['decMean']*u.degree)
+idx, d2d,d3d = cref.match_to_catalog_sky(cphot)
+print (phot)
+phott = np.transpose(phot)[idx]
+phot  = np.transpose (phott)
+
+
+
+distance = cref.separation (cphot[idx]).arcsecond
+
+condition = (distance < 5) & (phot['FLUX'] > 0) & (reftable['rMeanPSFMag'] > 0)& (reftable['rMeanPSFMag'] < 26)
+
+print ( distance )
+instmag =  -2.5 * np.log10(phot['FLUX'] / exptime)
+
+
+delta_r = ( reftable['rMeanPSFMag']  - instmag )[condition]
+catalog_gi = (reftable['gMeanPSFMag']- reftable['iMeanPSFMag'])[condition]
+catalog_r = reftable['rMeanPSFMag'][condition]
+
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+plt.plot (catalog_r, delta_r, '.')
+plt.xlim([10,22])
+plt.ylim ([20,26])
+
+photzp = np.median (delta_r)
+print ("Photometric zeropoint: %5.2f" % (photzp))
+
+plt.axhline(y=photzp, color='r', linestyle='-')
+
+plt.xlabel ("Reference catalog mag")
+plt.ylabel ("Reference Mag - Instrumnetal Mag")
+plt.title ("Photometric zeropoint %5.2f" % (photzp))
+plt.savefig ("photoplot_zp.png")
+
+
+plt.plot (catalog_gi, delta_r, '.')
+plt.xlim([-0.5,3])
+plt.ylim ([20,30])
+plt.savefig ("photoplot_color.png")
+
+
